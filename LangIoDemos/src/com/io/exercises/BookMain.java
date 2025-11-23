74ef81cd56714d78de5050f9fa61c65babca1d95
shristitechacademy <sripriyamathan@gmail.com>
2025-11-23 20:02:42 +0530

    io package

--- LangIoDemos/src/com/io/exercises/BookMain.java
+++ LangIoDemos/src/com/io/exercises/BookMain.java
@@ -1,5 +1,22 @@
 package com.io.exercises;
 
+import java.util.List;
+
 public class BookMain {
 
+	public static void main(String[] args) {
+		IBookConverter converter = new BookConverterImpl();
+		
+		//1st process
+		//call the method to get the csv file and convert to List
+		List<Book> books =  converter.convertToList("books.csv");
+		System.out.println(books);
+		
+		//2nd process
+		//serialize the books and save in a file - serializedFile
+		String serializedFile = converter.serializeBooks(books);
+		
+		//deserialize books - read the serialized file(serializedFile) and print the books
+		converter.deserializeBooks(serializedFile);
+	}
 }
